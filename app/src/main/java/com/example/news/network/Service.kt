55d0f12e20bd830be9e4e0aa923e2809472aa71b
Ítalo Bezerra <italobezerra.cw@gmail.com>
2023-04-27 06:43:09 -0300

    Unecessary file was removed

--- app/src/main/java/com/example/news/network/Service.kt
+++ app/src/main/java/com/example/news/network/Service.kt
@@ -6,6 +6,8 @@ import com.example.news.Constants
 import com.google.gson.GsonBuilder
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -13,6 +15,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.net.InetAddress
 
 
 interface GoogleNewsApiService {
@@ -34,4 +37,15 @@ object NewsNetwork {
 
 
     val newsService: GoogleNewsApiService by lazy { retrofit.create(GoogleNewsApiService::class.java) }
+}
+
+suspend fun isInternetAvailable(): Boolean {
+    return withContext(Dispatchers.IO) {
+        try {
+            val ipAddr: InetAddress = InetAddress.getByName("google.com")
+            !ipAddr.equals("")
+        } catch (e: Exception) {
+            false
+        }
+    }
 }
